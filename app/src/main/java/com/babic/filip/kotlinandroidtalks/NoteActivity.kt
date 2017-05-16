8130bba2fb8883da28d17d1339a73c6b0b4862e3
Filip <filip.babic@cobeisfresh.com>
2017-05-16 17:01:42 +0200

    Initial commit

---
+++ app/src/main/java/com/babic/filip/kotlinandroidtalks/NoteActivity.kt
@@ -0,0 +1,16 @@
+package com.babic.filip.kotlinandroidtalks
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Filip Babic @cobe
+ */
+class NoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes)
+
+    }
+}
---
+++ app/src/main/java/com/babic/filip/kotlinandroidtalks/data_objects/KotlinNote.kt
@@ -0,0 +1,6 @@
+package com.babic.filip.kotlinandroidtalks.data_objects
+
+/**
+ * Created by Filip Babic @cobe
+ */
+data class KotlinNote(var title: String, var text: String, var timestamp: String)
